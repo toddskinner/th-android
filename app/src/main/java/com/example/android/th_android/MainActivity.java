aad6d90dd35f60d20c0484e86c03fd73d3877a38
Todd Skinner <todd.skinner@gmail.com>
2016-10-10 20:41:10 -0700

    Add pic, review scores, icons, and scrolling

--- app/src/main/java/com/example/android/th_android/MainActivity.java
+++ app/src/main/java/com/example/android/th_android/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.th_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
 
